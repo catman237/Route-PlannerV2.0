926c12c3062deb6a217a8066aac3f4ac897d97bc
Greg <Ghughes610@gmail.com>
2021-10-27 12:14:25 -0600

    Hooking up google maps DirectionService component
    made state for DirectionService Component from @react-google-maps/api | hooked up text input fields to state with onChange()

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
 import "./App.scss";
+import { useState } from "react";
 import Header from "./Components/Header";
 import InputForm from "./Components/InputForm";
 import RouteContainer from "./Components/RouteContainer";
-import { DirectionsService, useJsApiLoader } from '@react-google-maps/api'
-
+import { DirectionsService, useJsApiLoader } from "@react-google-maps/api";
 
 function App() {
+  const [shop, setShop] = useState("");
+  const [stop, setStop] = useState("");
+  const [route, setRoute] = useState([]);
+  const [directionsInfo, setDirectionsInfo] = useState({});
+  const [directionsWaypoints, setDirectionWayPoints] = useState([]);
+  const [distances, setDistances] = useState([]);
 
-  const { isLoaded ,loadError } = useJsApiLoader({
-    googleMapsApiKey: 'this should be key from .env file'
-  })
+  const { isLoaded, loadError } = useJsApiLoader({
+    googleMapsApiKey: 'key goes here',
+  });
+
+  const setGoogleMapsRouteInfo = (shop, stop) => {
+    const directionsWaypoints = [
+      { location: stop },
+      { location: "700 golden ridge rd golden co" },
+      { location: "1740 30th St, Boulder, CO 80301" },
+    ];
+
+    const directionsInfo = {
+      origin: shop,
+      destination: shop,
+      travelMode: "DRIVING",
+      waypoints: directionsWaypoints,
+      optimizeWaypoints: true,
+      provideRouteAlternatives: false,
+      avoidFerries: true,
+      avoidHighways: false,
+      avoidTolls: false,
+    };
+  };
+  console.log(directionsInfo)
 
   return (
     <div className="App">
       <Header />
-      <InputForm />
+      <InputForm
+        setShop={setShop}
+        setStop={setStop}
+        setRoute={setRoute}
+        setDirectionsInfo={setDirectionsInfo}
+        setDistance={setDistances}
+        setGoogleMapsRouteInfo={setGoogleMapsRouteInfo}
+        stop={stop}
+        shop={shop}
+      />
       <RouteContainer />
-
-      <DirectionsService />
+      {isLoaded ? (
+        <DirectionsService
+          options={directionsInfo}
+          callback={(res) => {
+            console.log(res);
+          }}
+        />
+      ) : (
+        console.log(loadError)
+      )}
     </div>
   );
 }
--- src/Components/InputForm.jsx
+++ src/Components/InputForm.jsx
@@ -2,16 +2,29 @@ import React from "react";
 import Input from "@mui/material/Input";
 import Button from "@mui/material/Button";
 
-const InputForm = () => {
+const InputForm = ({
+  setShop,
+  shop,
+  setStop,
+  stop,
+  setDirectionsInfo,
+  setDistances,
+  setGoogleMapsRouteInfo
+}) => {
   return (
-    <div className='inputFormContainer'>
-      <div className='originInput'>
-        <Input defaultValue="Enter Origin" />
-        <Button>Submit</Button>
+    <div className="inputFormContainer">
+      <div className="StartPointInput">
+        <Input
+          placeholder="Enter Shop"
+          onChange={(e) => setShop(e.target.value)}
+        />
+        {/* <Button>Submit</Button> */}
       </div>
-      <div className='stopInput'>
-        <Input defaultValue="Enter a Stop" />
-        <Button>Submit</Button>
+      <div className="stopInput">
+        <Input placeholder="Enter a Stop" onChange={(e) => setStop(e.target.value)}/>
+      </div>
+      <div className="formSubmitButtonContainer">
+        <Button onClick={(shop, stop) => setGoogleMapsRouteInfo(shop, stop)}>Submit</Button>
       </div>
     </div>
   );
