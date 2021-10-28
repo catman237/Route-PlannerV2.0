bb5e2f6a3ce8c419de4903b93a5d92b175559a2c
Greg <Ghughes610@gmail.com>
2021-10-28 12:50:11 -0600

    edited git settings
    getting github to work again correctly, I edited the pull request setting for my git guardian. Small edit on cards

--- src/Components/Leg.jsx
+++ src/Components/Leg.jsx
@@ -1,7 +1,10 @@
-const Leg = ({leg}) => (
+const Leg = ({ leg }) => (
   <div className="legCard">
     <h1>Step: {leg.index + 1}</h1>
-    <div>Name: {leg.address}</div>
+    <div>
+      <span>Name: </span>
+      {leg.address}
+    </div>
     <div>Dist: {leg.distance}</div>
     <div>Time: {leg.time}</div>
   </div>
