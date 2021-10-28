60fd8393ae77bf1322e7493f9b03f717f47ecdbc
Greg <Ghughes610@gmail.com>
2021-10-28 15:31:03 -0600

    toggled complete and added date feature.
    The toggle card funtion is set to help show user that a step is completed. I also added a date to the Number of stops heading. Lastly I styled the routePlanner to allow all cards to be show no matter the amount of cards.

--- src/Components/Leg.jsx
+++ src/Components/Leg.jsx
@@ -1,25 +1,44 @@
-import BeenhereIcon from '@mui/icons-material/Beenhere';
+import { useState } from "react";
+import BeenhereIcon from "@mui/icons-material/Beenhere";
+import ClearIcon from "@mui/icons-material/Clear";
 
-const Leg = ({ leg }) => (
-  <div className="legCard">
+const Leg = ({ leg }) => {
+  const [toggle, setToggle] = useState(true);
 
-    <div id="infoContainerFirst">
-      <h1>Step {leg.index + 1}</h1>
-      <BeenhereIcon />
+  return (
+    <div className="legCard">
+      <div id="infoContainerFirst">
+        <h1>Step {leg.index + 1}</h1>
+
+        {toggle ? (
+          <BeenhereIcon
+            onClick={() => setToggle(!toggle)}
+            color="action"
+            className="checkButton"
+          />
+        ) : (
+          <BeenhereIcon
+            onClick={() => setToggle(!toggle)}
+            color="success"
+            className="checkButton"
+            fontSize="large"
+          />
+        )}
+      </div>
+      <div className="infoContainer">
+        <span className="label">Name: </span>
+        <span>{leg.address}</span>
+      </div>
+      <div className="infoContainer">
+        <span className="label">Distance: </span>
+        <span>{leg.distance} miles</span>
+      </div>
+      <div className="infoContainer">
+        <span className="label">Time: </span>
+        <span>{leg.time}</span>
+      </div>
     </div>
-    <div className="infoContainer">
-      <span className="label">Name: </span>
-      <span>{leg.address}</span>
-    </div>
-    <div className="infoContainer">
-      <span className="label">Distance: </span>
-      <span>{leg.distance} miles</span>
-    </div>
-    <div className="infoContainer">
-      <span className="label">Time: </span>
-      <span>{leg.time}</span>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Leg;
