739c9c8389142dc22eb9815830c5a54bbe43829e
Greg <Ghughes610@gmail.com>
2021-10-28 14:09:19 -0600

    card edits and destination function
    I implemented logic so a user cannot hit add destination button without a char in the destination inputt field. Also edited cards and added a check to them.

--- src/Components/Leg.jsx
+++ src/Components/Leg.jsx
@@ -1,17 +1,23 @@
+import BeenhereIcon from '@mui/icons-material/Beenhere';
+
 const Leg = ({ leg }) => (
   <div className="legCard">
-    <h1>Step: {leg.index + 1}</h1>
-    <div>
-      <span>Name: </span>
-      {leg.address}
+
+    <div id="infoContainerFirst">
+      <h1>Step {leg.index + 1}</h1>
+      <BeenhereIcon />
     </div>
-    <div>
-      <span>Distance:</span>
-      {leg.distance}
+    <div className="infoContainer">
+      <span className="label">Name: </span>
+      <span>{leg.address}</span>
     </div>
-    <div>
-      <span>Time: </span>
-      {leg.time}
+    <div className="infoContainer">
+      <span className="label">Distance: </span>
+      <span>{leg.distance} miles</span>
+    </div>
+    <div className="infoContainer">
+      <span className="label">Time: </span>
+      <span>{leg.time}</span>
     </div>
   </div>
 );
--- src/Components/RoutePlanner.jsx
+++ src/Components/RoutePlanner.jsx
@@ -9,46 +9,46 @@ const formatLeg = (leg, index) => {
     distance: leg.distance.text,
     time: leg.duration.text,
     address: leg.end_address,
-    index
+    index,
   };
 };
 
-// [
-//   {
-//     distance: 57,
-//     time: "1 hr 20 mins",
-//     address: "100 El Camino Real Belmont, CA 94002",
-//   },
-//   {
-//     distance: 57,
-//     time: "1 hr 20 mins",
-//     address: "100 El Camino Real Belmont, CA 94002",
-//   },
-// ]
-
 const RoutePlanner = () => {
-  const [origin, setOrigin] = useState("Golden 700 Golden Ridge Rd. Golden, CO 80401");
+  const [origin, setOrigin] = useState(
+    "Golden 700 Golden Ridge Rd. Golden, CO 80401"
+  );
   const [currentDestination, setCurrentDestination] = useState("");
   const [waypoints, setWayPoints] = useState([]);
-  const [waypointCount, setwaypointCount] = useState(0)
+  const [waypointCount, setwaypointCount] = useState(0);
   const [directionServiceOptions, setDirectionServiceOptions] = useState();
-  const [response, setResponse] = useState();
+  const [response, setResponse] = useState([
+    {
+      distance: 57,
+      time: "1 hr 20 mins",
+      address: "100 El Camino Real Belmont, CA 94002",
+    },
+    {
+      distance: 57,
+      time: "1 hr 20 mins",
+      address: "100 El Camino Real Belmont, CA 94002",
+    },
+  ]);
 
   const { isLoaded, loadError } = useJsApiLoader({
-    googleMapsApiKey: "key",
+    googleMapsApiKey: "AIzaSyAnorp7rnQdhLsXfPbi1pHj3uJmtORrP1E",
   });
 
   const addDestination = (dest) => {
-    waypoints.push({ location: dest });
+    dest.length > 1 && waypoints.push({ location: dest });
     setWayPoints(waypoints);
   };
 
   console.log(waypoints);
-  
+
   return (
     <div className="RoutePlannerContainer">
       <div className="title">
-        <h1>Route Planner</h1>
+        <h1>Number of Stops {waypointCount}</h1>
       </div>
       <Input
         placeholder="Origin"
@@ -61,12 +61,12 @@ const RoutePlanner = () => {
           onChange={(e) => setCurrentDestination(e.target.value)}
           value={currentDestination}
         />
-        <div>Destination Count: {waypointCount}</div>
+        
       </div>
       <Button
         onClick={() => {
           addDestination(currentDestination);
-          setwaypointCount(waypoints.length)
+          setwaypointCount(waypoints.length);
           setCurrentDestination("");
         }}
       >
@@ -87,7 +87,6 @@ const RoutePlanner = () => {
           };
           setDirectionServiceOptions(directionsInfo);
           setOrigin("");
-          
         }}
       >
         Plan Route
@@ -97,7 +96,9 @@ const RoutePlanner = () => {
         <DirectionsService
           options={directionServiceOptions}
           callback={(res) => {
-            setResponse(res.routes[0].legs.map((leg, index) => formatLeg(leg, index)));
+            setResponse(
+              res.routes[0].legs.map((leg, index) => formatLeg(leg, index))
+            );
           }}
         />
       )}
