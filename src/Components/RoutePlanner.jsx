8261781322cdcffa4034b296646378462e9d8d37
Greg <Ghughes610@gmail.com>
2021-10-28 16:40:26 -0600

    Documentation
    Created comments describing code in the files. Also wrote the README on how to start the application in the previous commit

--- src/Components/RoutePlanner.jsx
+++ src/Components/RoutePlanner.jsx
@@ -3,8 +3,9 @@ import LegContainer from "./LegContainer";
 import { DirectionsService, useJsApiLoader } from "@react-google-maps/api";
 import Button from "@mui/material/Button";
 import Input from "@mui/material/Input";
-import { QrCodeScannerOutlined } from "@mui/icons-material";
 
+// formatLeg is a here to help parse through the repsonse we get back from DirectionServices Component
+// If you need more data fill it out below and it will be taken from the repsonse
 const formatLeg = (leg, index) => {
   return {
     distance: leg.distance.text,
@@ -14,6 +15,8 @@ const formatLeg = (leg, index) => {
   };
 };
 
+// below is test data that can be placed into the response state to make sure things fit in that containter.
+// using this will aviod unnecessary api call to.
 const testData = [
   {
     distance: 57,
@@ -53,27 +56,29 @@ const testData = [
 ];
 
 const RoutePlanner = () => {
-  const [origin, setOrigin] = useState(
-    "Golden 700 Golden Ridge Rd. Golden, CO 80401"
-  );
+  const [origin, setOrigin] = useState("");
   const [currentDestination, setCurrentDestination] = useState("");
   const [waypoints, setWayPoints] = useState([]);
   const [waypointCount, setwaypointCount] = useState(0);
   const [directionServiceOptions, setDirectionServiceOptions] = useState();
-  const [response, setResponse] = useState();
+  const [response, setResponse] = useState(testData);
 
+  // useJsApiLoader is how the directionsService Component cn call to the @react-google-maps/api
+  // you have to use it in this format. put some logic in to show that is loaded is true and it will run to give you a response
   const { isLoaded, loadError } = useJsApiLoader({
     googleMapsApiKey: "key",
   });
 
+  //this function will update the wayPoints array
+  //the wayPoints array is sent through to the api and it is what stops we have to take between out origin and destination
   const addDestination = (dest) => {
     dest.length > 1 && waypoints.push({ location: dest });
     setWayPoints(waypoints);
   };
 
   // creating a date and formatting it to month / day / year
+  // slicing the day of week off of the response if you want the day start at index 0
   const date = Date().toString().slice(3, 15);
-  
 
   return (
     <div className="RoutePlannerContainer">
@@ -107,10 +112,12 @@ const RoutePlanner = () => {
       <Button
         onClick={() => {
           const directionsInfo = {
-            origin, 
+            origin,
             destination: origin,
             travelMode: "DRIVING",
+            // way points takes an array of objects they object must have the key of location
             waypoints,
+            // optimizeWayPoints will give you the most efficient route through the way points if it is set to true
             optimizeWaypoints: true,
             provideRouteAlternatives: false,
             avoidFerries: true,
@@ -129,6 +136,8 @@ const RoutePlanner = () => {
           options={directionServiceOptions}
           callback={(res) => {
             setResponse(
+              // accessing the needed data from the response
+              // index is used to keep track of how many stops have been requested.
               res.routes[0].legs.map((leg, index) => formatLeg(leg, index))
             );
           }}
